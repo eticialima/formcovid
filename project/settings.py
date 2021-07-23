420eb53c1145a7e4adefbcaeba4f8960d10d076b
eticialima <contato.leticialima.me@gmail.com>
2021-07-22 21:40:46 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -74,19 +74,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sistema',
-        'PORT': '3306',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '',
-
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
